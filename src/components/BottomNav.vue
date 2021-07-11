22de87bb2ae38bca295a07487c531f5dc9667a04
dannyhuang <kuanhsuh@gmail.com>
2021-07-11 08:22:38 +0800

    Feature - BottomNav Styling

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,24 +1,25 @@
 <template>
-  <v-bottom-navigation>
-    <v-btn to="/contact">
+  <v-bottom-navigation grow :value="value"
+    color="primary">
+    <v-btn to="/contact" text  height="100%">
       <span>聯絡</span>
 
       <v-icon>mdi-map-marker</v-icon>
     </v-btn>
 
-    <v-btn to="/menu">
+    <v-btn to="/menu" text height="100%">
       <span>菜單</span>
 
       <v-icon>mdi-heart</v-icon>
     </v-btn>
 
-    <v-btn to="/">
+    <v-btn to="/" text height="100%">
       <span>商店</span>
 
       <v-icon>mdi-heart</v-icon>
     </v-btn>
 
-    <v-btn to="/cart">
+    <v-btn to="/cart" text height="100%">
       <span>購物車</span>
 
       <v-icon>mdi-map-marker</v-icon>
